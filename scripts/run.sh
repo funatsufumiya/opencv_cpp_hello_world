3001d1e5aa75d8e07ba19684e2d604a831fd6a3d
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-02-28 12:10:48 +0900

    fix: update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,38 +6,20 @@ if [ ! -d "build" ]; then
     exit 1
 fi
 
-# check is win git-bash
-# if [ "$(uname -o)" = "Msys" ]; then
-
-# check is msys2
-if [ "$(uname -o)" = "Msys" ]; then
-    IS_MSYS2=true
-else
-    IS_MSYS2=false
-fi
-
-# check is git-bash
-if [ -z "$EXEPATH" ]; then
-    IS_GITBASH=false
-else
-    exe_path=$(cygpath "$EXEPATH")
-    if [ "$exe_path" = '/' ]; then
-        IS_GITBASH=true
-    else
-        IS_GITBASH=false
-    fi
-fi
-
-if [ "$IS_GITBASH" = true ]; then
-    echo "git-bash detected."
+# run with checking files
+if [ -f "build/Debug/opencv_hello_world.exe" ]; then
     cd build
     ./Debug/opencv_hello_world.exe
-elif [ "$IS_MSYS2" = true ]; then
-    echo "msys2 detected."
+    exit 0
+elif [ -f "build/opencv_hello_world.exe" ]; then
     cd build
-    ./Debug/opencv_hello_world.exe
-    # ./opencv_hello_world.exe
-else
+    ./opencv_hello_world.exe
+    exit 0
+elif [ -f "build/opencv_hello_world" ]; then
     cd build
     ./opencv_hello_world
+    exit 0
+else
+    echo "No executable file found. exit." > /dev/stderr
+    exit 1
 fi
